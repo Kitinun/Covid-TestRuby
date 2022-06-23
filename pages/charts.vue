1c42ddfafc34f72da0a82c61b34e320d00056f46
gitinun55 <kitinun@kkumail.com>
2022-06-24 00:46:05 +0700

    search datepicker

---
+++ pages/charts.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div class="px-2">
+            <h1>Graph Covid</h1>
+            <b-button variant="info" href="/">Go to view charts</b-button>
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'GraphPage',
+    data() {
+        return {
+            graphCovid: [],
+        }
+    },
+    async fetch() { 
+        this.covidData = await this.$axios.$get('https://disease.sh/v3/covid-19/historical?lastday=30');
+    },
+    methods: {
+    }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,66 @@
 <template>
-  <Tutorial/>
+  <div>
+    <div class="px-2">
+      <h1>Total Covid</h1>
+      <b-button variant="info" href="/charts">Go to view Graph</b-button>
+      <div class="mt-2">
+        <b-datepicker v-model="dateToday" @input="getCovidD"></b-datepicker>
+      </div>
+      <div class="mt-2">
+        <b-table striped hover :items="this.covidPickedData"></b-table>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data() {
+    return {
+      covidData: [],
+      covidPickedData: [],
+      dateToday: new Date().toString(),
+    }
+  },
+  async fetch() { 
+    this.covidData = await this.$axios.$get(
+      'https://disease.sh/v3/covid-19/historical?lastday=30'
+    );
+    
+    const dateFormatJHUCSSE = new Date()
+    const format = `${
+      dateFormatJHUCSSE.getMonth() + 1
+    }/${dateFormatJHUCSSE.getDate()}/${dateFormatJHUCSSE.getFullYear() % 100}`
+
+    this.covidPickedData = this.covidData.map(d => {
+        return { 
+          country: d.country,
+          province: d.province ?? '-',
+          case: d.timeline.cases[format],
+          deaths: d.timeline.deaths[format],
+          recovered: d.timeline.recovered[format]
+        }
+      })
+  },
+  methods: {
+    getCovidD() {
+      const dateFormatJHUCSSE = new Date(this.dateToday)
+
+      const format = `${
+        dateFormatJHUCSSE.getMonth() + 1
+      }/${dateFormatJHUCSSE.getDate()}/${dateFormatJHUCSSE.getFullYear() % 100}`
+
+      this.covidPickedData = this.covidData.map(d => {
+        return { 
+          country: d.country,
+          province: d.province ?? '-',
+          case: d.timeline.cases[format],
+          deaths: d.timeline.deaths[format],
+          recovered: d.timeline.recovered[format]
+        }
+      })
+    },
+  }
 }
 </script>
